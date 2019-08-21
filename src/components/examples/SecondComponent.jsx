05c3d3e03841b9ea6b2d85e4db2461a1a15fd754
Jason <jwilcox@ysgco.com>
2019-08-21 13:21:55 -0600

    Seperate Modules

---
+++ src/components/examples/SecondComponent.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class SecondComponent extends React.Component {
+    render() {
+      return(
+      <div className="secondComponent">
+        Second Component, here!
+        
+      </div>
+      )
+    }
+  }
